c7aa9a2424648ec9cadbdd2f7571cb51ecb39ea8
Ridge Reventar <ridge.reventar@gmail.com>
2019-03-27 23:32:01 -0400

    updated navbar with active styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
 import '../styles/Navbar.css';
 
@@ -7,10 +7,10 @@ const Navbar = () => {
     return (
         <div className="nav-container">
             <ul>
-                <li><Link to="/account/announcements">ANNOUNCEMENTS</Link></li>
-                <li><Link to="/account/projects">CLUB PROJECTS</Link></li>
-                <li><Link to="/account/films">FINISHED FILMS</Link></li>
-                <li><Link to="/account/events">EVENT CALENDAR</Link></li>
+                <li><NavLink className="nav-link" activeStyle={{ color: '#C89F3C' }} to="/account/announcements"  style={{ textDecoration: 'none' }}>ANNOUNCEMENTS</NavLink></li>
+                <li><NavLink className="nav-link" activeStyle={{ color: '#C89F3C' }} to="/account/projects" style={{ textDecoration: 'none' }}>CLUB PROJECTS</NavLink></li>
+                <li><NavLink className="nav-link" activeStyle={{ color: '#C89F3C' }} to="/account/films" style={{ textDecoration: 'none' }}>FINISHED FILMS</NavLink></li>
+                <li><NavLink className="nav-link" activeStyle={{ color: '#C89F3C' }} to="/account/events" style={{ textDecoration: 'none' }}>EVENT CALENDAR</NavLink></li>
             </ul>
         </div>
     );
