d19a0c12d79268e2e9db1280a3457d04eb1a401b
Ridge Reventar <ridge.reventar@gmail.com>
2019-03-29 06:26:50 -0400

    added events styling

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -5,8 +5,9 @@ class Events extends Component {
     state = {  }
     render() { 
         return (  
-
-            <iframe src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=b4hphe0q7oq285759aa1e5o03c%40group.calendar.google.com&amp;color=%232F2F2F&amp;ctz=America%2FToronto" style={{'border-width':'0'}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            <div className="events-container" >
+                <iframe src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=b4hphe0q7oq285759aa1e5o03c%40group.calendar.google.com&amp;color=%232F2F2F&amp;ctz=America%2FToronto" style={{'border-width':'0'}} width="700" height="500" frameBorder="0" scrolling="no"></iframe>
+            </div>
         );
     }
 }
