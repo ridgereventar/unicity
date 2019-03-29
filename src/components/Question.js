493460a4a579d741bef32a7fb8717978e0999b6e
Ridge Reventar <ridge.reventar@gmail.com>
2019-03-29 06:12:10 -0400

    Added question send msg

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
 import '../styles/Question.css';
 
-const Question = () => {
-    return (
-        <div className="question-container">
-            <h1 id="questions-label">Questions?</h1>
-            <textarea id="question-text-area" placeholder="Let us know!"></textarea>
-            <br></br>
-            <button id="question-send-button">Send</button>
-        </div>
-    );
-    
-};
+class Question extends Component {
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        $('#question-form')[0].reset();
+        $('#question-sent-msg').fadeIn(1000);
+        $('#question-sent-msg').delay(1000).fadeOut(500);
+
+    }
+    render() { 
+        return (
+            <div className="question-container">
+                <h1 id="questions-label">Questions?</h1>
+                <form id="question-form" onSubmit={this.handleSubmit}>
+                    <textarea id="question-text-area" placeholder="Let us know!"></textarea>
+                    <label id="question-sent-msg"> Question has been sent </label>
+                    <input id="question-send-button" type="submit" value="SEND" />
+
+                </form>
+            </div>
+        );    
+    }
+}
+ 
 
 export default Question;
